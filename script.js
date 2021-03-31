4c25984b16daed9fc62cc67cce153b6de9a51632
Gabrielle Pugh <gabriellepugh30@gmail.com>
2021-03-30 22:14:40 -0400

    Changed the name spelling

--- script.js
+++ script.js
@@ -5,16 +5,15 @@ function marinaraSauce(cookName) {
     console.log("Hey, " + cookName + "! This is my marinara sauce recipe!");
     console.log("- Heat a medium-large saucepan over medium heat.")
     console.log();
-    console.log("- Add" + two + "tspns of oil and 5 carlic cloves, cook until golden, about 2 minutes")
+    console.log("- Add " + two + "tspns of oil and 5 carlic cloves, cook until golden, about 2 minutes")
     console.log();
-    console.log("- Add" + oneFourth + "cup water, " + two + "cans of crushed tomates, and salt and season with black pepper")
+    console.log("- Add " + oneFourth + "cup water, " + two + " cans of crushed tomates, and salt and season with black pepper")
     console.log();
     console.log("- Cover the pot, bring to a boil, reduce the heat to medium low, and simmer until warm")
     console.log();
-    console.log("- Stir in " + oneFourth + "cup roughly chopped fresh basil, salt and pepper as needed")
-    console.log();
+    console.log("- Stir in " + oneFourth + " cup roughly chopped fresh basil, salt and pepper as needed")
 
 }
 
-marinaraSauce("Adam");
+marinaraSauce("Gabi");
 

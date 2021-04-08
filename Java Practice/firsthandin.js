05c615496ec118cda0f59c73b87f706606ddc9dc
Gabrielle Pugh <gabriellepugh30@gmail.com>
2021-04-07 21:44:06 -0400

    new changes

---
+++ Java Practice/firsthandin.js
@@ -0,0 +1,64 @@
+function printNumbers() {
+
+for (var input = 1; input <= 10; input++) {
+    console.log(input);
+   }
+}
+printNumbers();
+
+
+var yourName = "Print your name";
+var favoriteSubject = "Print your favorite subject";
+function madlib() {
+
+}
+
+
+function tipAmount() {
+    if (tipAmount > 20) {
+        console.log(20, "good")
+    }
+    if (tipAmount > 15) {
+        console.log(15, "fair")
+    }
+    if (tipAmount > 10) {
+        console.log(10, "bad")
+    }
+}
+tipAmount();
+
+
+ function totalAmount() {
+     if (totalAmount, 100, "good") {
+         console.log(120)
+     }
+     if (totalAmount, 40, "fair") {
+         console.log(46)
+     }
+ }
+
+var startNumber = 1;
+var endNumber = 10;
+var total = " ";
+function printNumbers(startNumber, endNumber) {
+    for(var start=startNumber; start < endNumber ;start++) {
+        total =total + start + " ";
+    }
+}
+
+function printSquare() {
+    var size = 5,
+    i, j;
+console.log("Gabby")
+for (i = 0; i < size; i++) {
+    for (j = 0; j < size; j++) {
+        console.log("*");
+    }
+    console.log(printSquare)
+}
+}
+
+printSquare()
+
+
+console.log("hi");
